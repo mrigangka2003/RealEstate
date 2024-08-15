d617e231aa66640aec129734959ae04eee2344d4
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-08-16 02:28:13 +0530

    Updated routes file structure

---
+++ client/src/routes/index.js
@@ -0,0 +1,21 @@
+import HomePage from "./homePage/homePage";
+import { Layout,RequireAuth } from "./layout/layout";
+import ListPage from "./listPage/listPage";
+import Login from "./login/login";
+import Register from "./register/register";
+import ProfilePage from "./profilePage/profilePage";
+import ProfileUpdatePage from "./profileUpdatePage/profileUpdatePage";
+import SinglePage from "./singlePage/singlePage";
+
+
+export {
+    HomePage,
+    Layout,
+    RequireAuth,
+    ListPage,
+    Login,
+    Register,
+    ProfilePage,
+    ProfileUpdatePage,
+    SinglePage
+}
