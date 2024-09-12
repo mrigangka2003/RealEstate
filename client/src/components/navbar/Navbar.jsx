43c2ff8180329bb0ef15ab249312e98e24613789
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-09-13 00:51:07 +0530

    changes a to link and link to navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState,useContext } from "react";
 import "./navbar.scss";
-import { Link } from "react-router-dom";
+import { Link,NavLink } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
@@ -11,14 +11,15 @@ function Navbar() {
   return (
     <nav>
       <div className="left">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
           <img src="/logo.png" alt="" />
           <span>LamaEstate</span>
-        </a>
-        <Link to={"/home"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contact"}>Contact</Link>
-        <Link to={"/agents"}>Agents</Link>
+        </Link>
+
+        <NavLink to={'/home'}>Home</NavLink>
+        <NavLink to={'/about'}>About</NavLink>
+        <NavLink to={'/contact'}>Contact</NavLink>
+        <NavLink to={'/agents'}>Agents</NavLink>
       </div>
       <div className="right">
         {currentUser ? (
@@ -49,12 +50,12 @@ function Navbar() {
           />
         </div>
         <div className={open ? "menu active" : "menu"}>
-          <a href="/">Home</a>
-          <a href="/">About</a>
-          <a href="/">Contact</a>
-          <a href="/">Agents</a>
-          <a href="/">Sign in</a>
-          <a href="/">Sign up</a>
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/agents">Agents</Link>
+          <Link to="/login">Sign in</Link>
+          <Link to="/register">Sign up</Link>
         </div>
       </div>
     </nav>
