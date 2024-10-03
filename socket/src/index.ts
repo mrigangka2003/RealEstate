4153f2d7823a1aa0d5e23c782632d0c36d484052
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-10-03 18:20:31 +0530

    setting up sockets

---
+++ socket/src/index.ts
@@ -0,0 +1,13 @@
+import express,{Request,Response} from "express" ;
+import {Server} from "socket.io" ;
+
+
+const io = new Server({
+    cors:{
+        origin : "http:localhost:5173"
+    }
+})
+
+io.on("connection" , (socket) =>{
+    console.log(socket) ;
+})
